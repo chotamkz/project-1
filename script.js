b9de6c4e099bf8a3132b9dbdb71a72f53854324e
Daniyar <daniknurlanov0509@gmail.com>
2022-07-29 20:33:35 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// alert("Hi");
+ //const result = confirm("Are you here?");
+// console.log(result);
+
+//const a = -prompt("Вам есть 18?");
+//console.log(a+2);
+
+//const b = [];
+//b[0] = prompt("Как тебя зовут?");
+//b[1] = prompt("Твоя фамилия?");
+//b[2] = alert("Вам есть 18?");
+
+//document.write(b);
+
+const user = "Ivan";
+alert(`Hi, ${user}`);
